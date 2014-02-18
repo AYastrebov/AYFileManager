624e8e7a0caaa78338e0029abc49c92fb90e393d
Andrey Yastrebov <ayastrebov@gmail.com>
2014-02-18 16:38:32 +0400

    added zip submodule

--- AYFileManager/AYFileManager+ZIP.m
+++ AYFileManager/AYFileManager+ZIP.m
@@ -7,26 +7,26 @@
 //
 
 #import "AYFileManager+ZIP.h"
-//#import "SSZipArchive.h"
+#import "SSZipArchive.h"
 
 @implementation AYFileManager (ZIP)
 
-//+ (void)unzipFileAtPath:(NSString *)path toDestination:(NSString *)destination handler:(void (^)(BOOL success))block
-//{
-//    BOOL result = [SSZipArchive unzipFileAtPath:path toDestination:destination];
-//    if (block)
-//    {
-//        block(result);
-//    }
-//}
-//
-//+ (void)createZipFileAtPath:(NSString *)path withFilesAtPaths:(NSArray *)filenames handler:(void (^)(BOOL success))block
-//{
-//    BOOL result = [SSZipArchive createZipFileAtPath:path withFilesAtPaths:filenames];
-//    if (block)
-//    {
-//        block(result);
-//    }
-//}
++ (void)unzipFileAtPath:(NSString *)path toDestination:(NSString *)destination handler:(void (^)(BOOL success))block
+{
+    BOOL result = [SSZipArchive unzipFileAtPath:path toDestination:destination];
+    if (block)
+    {
+        block(result);
+    }
+}
+
++ (void)createZipFileAtPath:(NSString *)path withFilesAtPaths:(NSArray *)filenames handler:(void (^)(BOOL success))block
+{
+    BOOL result = [SSZipArchive createZipFileAtPath:path withFilesAtPaths:filenames];
+    if (block)
+    {
+        block(result);
+    }
+}
 
 @end
